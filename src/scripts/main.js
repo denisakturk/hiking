accfa7ef265d95c1a775ed23e6fed170c52701c0
Denis Akturk <denisakturk@yandex.ru>
2020-07-25 16:23:57 +0300

    style

---
+++ src/scripts/main.js
@@ -0,0 +1,6 @@
+heightSetter();
+window.addEventListener('resize', heightSetter);
+function heightSetter() {
+  document.getElementById("hero").style.height = window.innerHeight  + 'px';
+  // document.getElementById("promo").style.height = window.innerHeight  + 'px';
+}
